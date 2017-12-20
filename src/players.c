ba01b2ff986c99068ebb10f18f13ffb42b1da844
Adrian MARANDIUC <amarandi@student.42.fr>
2017-12-20 14:50:38 +0100

    debugging

--- src/players.c
+++ src/players.c
@@ -6,7 +6,7 @@
 /*   By: kyazdani <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/16 23:06:56 by kyazdani          #+#    #+#             */
-/*   Updated: 2017/12/19 20:27:26 by amarandi         ###   ########.fr       */
+/*   Updated: 2017/12/19 21:55:05 by amarandi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,74 +49,4 @@ int	check(t_map *map, int force, char c)
 		x++;
 	}
 	return (0);
-}
-
-int	p_hard2(t_map *map)
-{
-	int		x;
-	int		y;
-	char	c;
-
-	c = (map->c == 'O' ? 'X' : 'O');
-	x = 0;
-	while (x < map->xmax)
-	{
-		y = 0;
-		while (map->tab[y + 1] && map->tab[y + 1][x] == '.')
-			y++;
-		map->x = x;
-		map->y = y;
-		if (horizontal(*map, 3, c) || diagonal(*map, 3, c) ||
-				vertical(*map, 3, c) || adiagonal(*map, 3, c))
-			if ((placer_piece(map, x)) >= 0)
-				return (1);
-		x++;
-	}
-	return (p_hard2(map));
-}
-
-int	p_med(t_map *map)
-{
-	int		x;
-	int		y;
-
-	x = 0;
-	while (x < map->xmax)
-	{
-		y = 0;
-		while (map->tab[y + 1] && map->tab[y + 1][x] == '.')
-			y++;
-		map->x = x;
-		map->y = y;
-		if (horizontal(*map, 2, map->c) || diagonal(*map, 2, map->c) ||
-				vertical(*map, 2, map->c) || adiagonal(*map, 2, map->c))
-			if ((placer_piece(map, x)) >= 0)
-				return (1);
-		x++;
-	}
-	return (p_easy(map));
-}
-
-int	p_med2(t_map *map)
-{
-	int		x;
-	int		y;
-	char	c;
-
-	c = (map->c == 'O' ? 'X' : 'O');
-	x = 0;
-	while (x < map->xmax)
-	{
-		y = 0;
-		while (map->tab[y + 1] && map->tab[y + 1][x] == '.')
-			y++;
-		map->x = x;
-		map->y = y;
-		if (horizontal(*map, 2, c) || diagonal(*map, 2, c) ||
-				vertical(*map, 2, c) || adiagonal(*map, 2, c))
-			if ((placer_piece(map, x)) >= 0)
-				return (1);
-		x++;
-	}
-	return (p_med(map));
 }
